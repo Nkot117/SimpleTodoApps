a12ce4f9a7ac6b27ff8a03ab9dfb16706a9fea01
Naoto Kumakura <kumakura117@gmail.com>
2023-05-21 11:13:28 +0900

    VIewBaindingを有効化

--- app/src/main/java/com/websarva/wings/android/simpletodoapps/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/simpletodoapps/MainActivity.kt
@@ -2,10 +2,21 @@ package com.websarva.wings.android.simpletodoapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.websarva.wings.android.simpletodoapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.fab.setOnClickListener{
+            Toast.makeText(this@MainActivity,"fabクリック",Toast.LENGTH_SHORT).show()
+        }
     }
 }
